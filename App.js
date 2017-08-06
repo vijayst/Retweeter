2f5618688212fc3ff0a7f8fa926b734f8fb1ae48
vijayst <vijayst@gmail.com>
2017-08-06 22:20:40 +0530

    display in flatlist

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, FlatList, Button } from 'react-native';
 import firebase from './firebase';
 
 export default class App extends React.Component {
     state = {
         isQuerySet: false,
-        query: ''
+        query: '',
+        tweets: []
     }
 
     componentDidMount() {
         firebase.messaging().getToken()
             .then((token) => {
-                console.warn('Device FCM Token: ', token);
+                // console.warn('Device FCM Token: ', token);
             });
     }
 
     handleSetPress() {
         fetch(`https://fvmylcig0b.execute-api.us-west-2.amazonaws.com/prod/tweets?query=${encodeURIComponent(this.state.query)}`)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            this.setState({ isQuerySet: true });
-        });
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    isQuerySet: true,
+                    tweets: json
+                });
+            });
+    }
+
+    handleRetweet() {
+
     }
 
     render() {
         return (
             <View style={styles.container}>
                 {this.state.isQuerySet ? (
-                    <Text>Tweets in the last one hour</Text>
-                ) : (
-                    <View style={styles.row}>
-                        <TextInput 
-                            style={styles.input}
-                            value={this.state.query}
-                            onChangeText={query => this.setState({ query })}
+                    <View style={styles.column}>
+                        <Text style={styles.heading}>Tweets in the last one hour</Text>
+                        <FlatList
+                            data={this.state.tweets}
+                            keyExtractor={(item, index) => index}
+                            renderItem={({ item }) => (
+                                <View style={{ marginBottom: 20 }}>
+                                    <Text style={styles.name}>{item.name}</Text>
+                                    <Text style={styles.text}>{item.text}</Text>
+                                    <Button 
+                                        title="Retweet"  
+                                        onPress={this.handleRetweet.bind(this)}
+                                    />
+                                </View>
+                            )}
                         />
-                        <TouchableHighlight style={styles.button} onPress={this.handleSetPress.bind(this)}>
-                            <Text style={styles.buttonText}>Set</Text>
-                        </TouchableHighlight>
                     </View>
-                )}
+                ) : (
+                        <View style={styles.row}>
+                            <TextInput
+                                style={styles.input}
+                                value={this.state.query}
+                                onChangeText={query => this.setState({ query })}
+                            />
+                            <TouchableHighlight style={styles.button} onPress={this.handleSetPress.bind(this)}>
+                                <Text style={styles.buttonText}>Set</Text>
+                            </TouchableHighlight>
+                        </View>
+                    )}
             </View>
         );
     }
@@ -59,6 +82,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         margin: 20
     },
+    column: {
+        justifyContent: 'space-between',
+        margin: 20
+    },
     input: {
         flex: 1,
         fontSize: 20
@@ -74,5 +101,23 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#FFF',
         fontSize: 20
+    },
+    heading: {
+        backgroundColor: '#0047AB',
+        color: '#FFF',
+        fontSize: 20,
+        padding: 5
+    },
+    name: {
+        backgroundColor: '#ccc',
+        fontSize: 18,
+        padding: 5
+    },
+    text: {
+        fontSize: 14,
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        marginBottom: 10
     }
 });
